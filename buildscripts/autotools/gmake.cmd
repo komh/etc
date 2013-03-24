aa7782504d4d49ff3666f571afc08fad398d3703
KO Myung-Hun <komh@chollian.net>
2013-03-24 18:14:53 +0900

    Add scripts to build autotools

---
+++ buildscripts/autotools/gmake.cmd
@@ -0,0 +1,20 @@
+extproc sh
+
+# convert backslashes of PATH to slashes
+IFS="\\"
+TEMP_PATH=
+for dir in $PATH; do
+    if test -z "$TEMP_PATH"; then
+        TEMP_PATH="$dir"
+    else
+        TEMP_PATH="$TEMP_PATH/$dir"
+    fi
+done
+PATH="$TEMP_PATH"
+export PATH
+unset TEMP_PATH
+
+export ac_executable_extensions=".exe"
+
+make.exe SHELL=/bin/sh $*
+
