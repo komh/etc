c7862dab47c679aafcdb72d50b39d3902d2be185
KO Myung-Hun <komh@chollian.net>
2013-03-24 18:09:54 +0900

    Add a script to set up the build environments for perl v5.10.0

---
+++ buildenv/pl5env.cmd
@@ -0,0 +1,10 @@
+@echo off
+setlocal
+set PATH=f:\lang\perl5\bin;%PATH%
+set OLD_BEGINLIBPATH=%BEGINLIBPATH%
+set BEGINLIBPATH=f:\lang\perl5\bin;%BEGINLIBPATH%
+set PERLLIB_PREFIX=/perl5/lib;f:/lang/perl5/lib
+set PERL_SH_DIR=f:/bin
+%COMSPEC%
+set BEGINLIBPATH=%OLD_BEGINLIBPATH
+endlocal
