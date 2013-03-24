9abde0c038c7aa2dbbfdfffff59c68f42c939645
KO Myung-Hun <komh@chollian.net>
2013-03-24 18:09:39 +0900

    Add a script to set up the build environments for Qt4

---
+++ buildenv/qtenv.cmd
@@ -0,0 +1,5 @@
+call gcc4.cmd WLINK
+set MAKESHELL=e:\os2\cmd.exe
+set path=f:\lang\qt4\bin;%PATH%
+rem set beginlibpath=f:\lang\qt4\dll;%BEGINLIBPATH%
+set CPLUS_INCLUDE_PATH=.;%CPLUS_INCLUDE_PATH%
