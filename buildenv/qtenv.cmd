fc6638ad63c02576f08546ce8e3e8b2d3cd1d055
KO Myung-Hun <komh@chollian.net>
2014-08-28 16:56:56 +0900

    qtenv.cmd: allow to specify Qt dir
        modified:   buildenv/qtenv.cmd

--- buildenv/qtenv.cmd
+++ buildenv/qtenv.cmd
@@ -1,5 +1,8 @@
 call gcc4.cmd WLINK
 set MAKESHELL=e:\os2\cmd.exe
-set path=f:\lang\qt4\bin;%PATH%
-rem set beginlibpath=f:\lang\qt4\dll;%BEGINLIBPATH%
+set qtdir=%1
+if "%qtdir%" == "" set qtdir=f:\lang\qt4
+set path=%qtdir%\bin;%PATH%
+set beginlibpath=%qtdir%\bin;%BEGINLIBPATH%
+set qtdir=
 set CPLUS_INCLUDE_PATH=.;%CPLUS_INCLUDE_PATH%
