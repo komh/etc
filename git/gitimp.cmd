d9a9ef6c1de199d4bbb3b5d80bf1ffd65024e9aa
KO Myung-Hun <komh@chollian.net>
2014-08-28 17:05:23 +0900

    gitimp.cmd: copy .gitignore from checked-out sources if possible
        modified:   gitimp.cmd

--- git/gitimp.cmd
+++ git/gitimp.cmd
@@ -1,9 +1,33 @@
-if "%1" == "" goto error
-git init .
-git add .
-git ci -m %1
-git tag origin head
-goto done
-:error
-echo Specify a commit message for a import
-:done
+/* git import wrapper */
+parse arg '"' sImportMsg '"' sGitIgnore
+
+sImportMsg = strip(sImportMsg)
+sGitIgnore = strip(sGitIgnore)
+
+if sImportMsg = '' then
+do
+    say 'Specify a commit message for a import'
+    exit 1
+end
+
+if sGitIgnore = '' then
+do
+    sCurDir = directory()
+
+    iPos = lastpos('-', sCurDir)
+    if iPos > 0 then
+    do
+        sGitIgnore = substr(sCurDir, 1, iPos - 1) || '.git\.gitignore'
+        if stream(sGitIgnore, 'c', 'query exists') = '' then
+            sGitIngore = 'g:\myetc\.gitignore'
+    end
+end
+
+'copy' sGitIgnore .
+
+'git init .'
+'git add .'
+'git ci -m ' '"' || sImportMsg || '"'
+'git tag origin head'
+
+exit 0
