9272d5f2d3ef80a96c052e0a446189bcb926fe77
KO Myung-Hun <komh@chollian.net>
2016-01-19 15:01:53 +0900

    gmake.cmd: remove unnecessary statements
        modified:   gmake.cmd

--- buildscripts/gmake.cmd
+++ buildscripts/gmake.cmd
@@ -3,8 +3,6 @@ parse arg argall
 '@echo off'
 'setlocal'
 'set LANG=C'
-'set COMSPEC=/bin/sh'
-'rem make SHELL=/bin/sh' argall
 'set MAKESHELL=/bin/sh'
 'make' argall '2>&1 | tee gmake.log'
 ec = rc
