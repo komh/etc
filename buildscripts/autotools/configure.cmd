cff6f97edf58df9bb85575e73ebf2e0735067a1a
KO Myung-Hun <komh@chollian.net>
2017-01-11 16:24:36 +0900

    autotools: remove unnecessary codes
        modified:   buildscripts/autotools/configure.cmd
        modified:   buildscripts/autotools/gmake.cmd

--- buildscripts/autotools/configure.cmd
+++ buildscripts/autotools/configure.cmd
@@ -1,7 +1,5 @@
 extproc sh
 
-export COMSPEC=/bin/sh
-
 # convert backslashes of PATH to slashes
 export PATH=$(expr "$PATH" | tr '\\' /)
 
--- buildscripts/autotools/gmake.cmd
+++ buildscripts/autotools/gmake.cmd
@@ -1,11 +1,8 @@
 extproc sh
 
-export COMSPEC=/bin/sh
-
 # convert backslashes of PATH to slashes
 export PATH=$(expr "$PATH" | tr '\\' /)
 
 export ac_executable_extensions=".exe"
 
 make.exe SHELL=/bin/sh "$@"
-
