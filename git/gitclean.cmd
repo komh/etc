80a0aa0eefc906781d61f72815347f9902ae1546
KO Myung-Hun <komh@chollian.net>
2017-10-05 20:39:04 +0900

    gitclean.cmd: fix typo
        modified:   git/gitclean.cmd

--- git/gitclean.cmd
+++ git/gitclean.cmd
@@ -1,2 +1,2 @@
 @echo off
-find . -maxdepth 1 -not ( -regex \. -o -name .git -o -name .gitignore -o -name .gitattributes -o -name med.lru -o -name patches -o -name sp.cmd) | xargs rm -rf
+find . -maxdepth 1 -not ( -regex \. -o -name .git -o -name .gitignore -o -name .gitattributes -o -name med.lru -o -name patches -o -name sp.cmd ) | xargs rm -rf
