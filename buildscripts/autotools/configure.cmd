33cdd76904a6298a963f9af6689d58549cce4cd2
KO Myung-Hun <komh@chollian.net>
2017-11-04 15:14:35 +0900

    configure.cmd: print error messages if ./configure not found
        modified:   buildscripts/autotools/configure.cmd
        modified:   buildscripts/configure.cmd

--- buildscripts/autotools/configure.cmd
+++ buildscripts/autotools/configure.cmd
@@ -5,5 +5,5 @@ export PATH=$(expr "$PATH" | tr '\\' /)
 
 export ac_executable_extensions=".exe"
 
-test -f ./configure. &&
+test -f ./configure. || { echo "\`./configure' not found !!!"; exit 1; }
 ./configure "$@" 2>&1 | tee configure.log
