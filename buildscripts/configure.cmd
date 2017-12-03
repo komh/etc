6c3c5501cc87a2d51f1d179b8e40d811d303ec75
KO Myung-Hun <komh@chollian.net>
2017-12-03 13:02:44 +0900

    configure.cmd: add .log to the log file
        modified:   buildscripts/configure.cmd

--- buildscripts/configure.cmd
+++ buildscripts/configure.cmd
@@ -2,4 +2,4 @@ extproc sh
 
 n=configure
 test -f "./$n." || { echo "\`./$n' not found !!!"; exit 1; }
-"./$n" --disable-shared --enable-static "$@" 2>&1 | tee "$n"
+"./$n" --disable-shared --enable-static "$@" 2>&1 | tee "$n.log"
