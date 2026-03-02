75197d09a3a06ce3953dbcdf8af37877da5e7c11
KO Myung-Hun <komh78@gmail.com>
2026-03-02 21:52:48 +0900

    buildscripts: add conf.cmd
    This is a configure script for autotools.
    
        new file:   buildscripts/conf.cmd

---
+++ buildscripts/conf.cmd
@@ -0,0 +1,131 @@
+/* conf.cmd: a configure script for autotools */
+parse arg sBuildDir sArgs
+
+sOpts =,
+    '--prefix=/@unixroot/usr/local',
+    '--disable-shared',
+    '--enable-static'
+
+exit config( sBuildDir, sOpts, sArgs )
+
+/**
+ * Configure a build dir
+ */
+config: procedure
+    parse arg sBuildDir, sOpts, sArgs
+
+    /* Check parameters */
+    if left( sBuildDir, 1 ) == '-' then
+    do
+        sArgs = strip( sBuildDir sArgs )
+        sBuildDir = ''
+    end
+    sArgs = strip( sOpts sArgs )
+
+    /* Check a build dir */
+    if issrcdir( sBuildDir ) then
+    do
+        if sBuildDir \== '' then
+        do
+            say sBuildDir 'is a source directory!'
+
+            return 1
+        end
+
+        /* default build dir */
+        sBuildDir = 'build'
+    end
+
+    /* Find a top source dir */
+    sCurDir = directory()
+    sSrcDir = sCurDir
+    do while \ issrcdir( sSrcDir )
+        if length( sSrcDir ) < 4 then
+        do
+            /* Reached to a root directory */
+
+            say 'Could not find a source directory!'
+
+            /* Restore CWD */
+            call directory sCurDir
+
+            return 1
+        end
+
+        sSrcDir = directory( sSrcDir || '\..')
+    end
+
+    do while issrcdir( sSrcDir )
+        sTopSrcDir = sSrcDir
+
+        if length( sSrcDir ) < 4 then
+        do
+            /* Reached to a root directory */
+
+            leave
+        end
+
+        sSrcDir = directory( sSrcDir || '\..')
+    end
+
+    /* Restore CWD */
+    call directory sCurDir
+
+    /* if --help, skip CD */
+    if wordpos('--help', sArgs ) == 0 then
+    do
+        /* CD into a build dir */
+        if directory( sBuildDir ) == '' then
+        do
+            'mkdir' sBuildDir '2>nul'
+            if directory( sBuildDir ) == '' then
+            do
+                say 'Cannot change a directory to 'sBuildDir'!'
+
+                return 1
+            end
+        end
+
+        /* copy the configure script file */
+        parse source . . sMe
+        'copy' sMe '1>nul 2>nul'
+    end
+
+    call setlocal
+
+    /* For the cases using Make while configuring */
+    call value 'LANG', 'C', 'OS2ENVIRONMENT'
+    call value 'MAKESHELL', 'sh.exe', 'OS2ENVIRONMENT'
+
+    /* Convert '\' to '/' */
+    sTopSrcDir = translate( sTopSrcDir, '/', '\');
+
+    /* Launch configure */
+    'sh.exe' sTopSrcDir || '/configure' sArgs '2>&1 | tee conf.log'
+    ec = rc
+
+    call endlocal
+
+    return rc
+
+/**
+ * Check if the given build dir has been already configured
+ */
+isbuilddir: procedure
+    parse arg sDir
+
+    if sDir == '' then
+        sDir = '.'
+
+    return stream( strip( sDir, 'T', '\') || '\config.status', 'c', 'query exists') \== ''
+
+/**
+ * Check if the given dir is a source dir
+ */
+issrcdir: procedure
+    parse arg sDir
+
+    if sDir == '' then
+        sDir = '.'
+
+    return stream( strip( sDir, 'T', '\') || '\configure', 'c', 'query exists') \== ''
